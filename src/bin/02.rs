26ad63c334399cbc0b9e4ca579731f65f764718c
Lauri Seppäläinen <lauri.seppalainen@helsinki.fi>
2024-12-20 15:22:38 +0200

    2/2 done

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,48 +1,46 @@
 advent_of_code::solution!(2);
-use itertools::Itertools;
 
-fn parse_input(input: &str) -> Vec<Vec<u64>> {
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
     let mut vecs = Vec::new();
     for line in input.lines() {
         let nums = line.split_whitespace();
         let mut levels = Vec::new();
         for charnum in nums {
-            levels.push(charnum.parse::<u64>().unwrap());
+            levels.push(charnum.parse::<i32>().unwrap());
         }
         vecs.push(levels);
     }
     vecs
 }
 
+fn check_single_report(report: Vec<i32>) -> bool {
+    let mut decreasing = true;
+    let mut increasing = true;
+    for i in 0..report.len() - 1 {
+        // set flags for increase/decrease
+        let a = &report[i];
+        let b = &report[i + 1];
+        if decreasing && a >= b {
+            decreasing = false;
+        }
+        if increasing && b >= a {
+            increasing = false;
+        }
+        // check diff
+        let diff = (1..=3).contains(&a.abs_diff(*b));
+        if !diff || (!decreasing && !increasing) {
+            // recursively check single difference collections
+            return false;
+        }
+    }
+    true
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let reports = parse_input(input);
-
     let mut safe_reports = 0u64;
     for report in reports {
-        let mut decreasing = true;
-        let mut increasing = true;
-        let mut is_safe = true;
-        for (a, b) in report.into_iter().tuple_windows() {
-            // set flags for increase/decrease
-            if decreasing && a > b {
-                decreasing = false;
-            }
-            if increasing && b > a {
-                increasing = false;
-            }
-            // check diff
-            let diff = a.abs_diff(b);
-            if !(1..=3).contains(&diff) {
-                is_safe = false;
-                break;
-            }
-            // break if both not decreasing and not increasing
-            if !decreasing && !increasing {
-                is_safe = false;
-                break;
-            }
-        }
-        if is_safe {
+        if check_single_report(report) {
             safe_reports += 1;
         }
     }
@@ -50,7 +48,23 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let reports = parse_input(input);
+    let mut safe_reports = 0u64;
+    for report in reports.clone() {
+        if check_single_report(report.clone()) {
+            safe_reports += 1;
+        } else {
+            for ri in 0..report.len() {
+                let mut remove_i = report.clone();
+                remove_i.remove(ri);
+                if check_single_report(remove_i.clone()) {
+                    safe_reports += 1;
+                    break;
+                }
+            }
+        }
+    }
+    Some(safe_reports)
 }
 
 #[cfg(test)]
@@ -60,12 +74,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(2));
+        assert_eq!(result, Some(0));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(15));
     }
 }
