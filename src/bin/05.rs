64dac916fd6e40607b21d0b2bd84e1e175a0b49e
Lauri <you@example.com>
2024-12-21 13:32:30 +0200

    5/1 done

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,7 +1,92 @@
+use itertools::Itertools;
+use std::fmt;
+
 advent_of_code::solution!(5);
 
+struct Rule {
+    first: u8,
+    second: u8,
+}
+
+impl fmt::Debug for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Rule({}, {})", self.first, self.second)
+    }
+}
+
+fn parse_input(input: &str) -> (Vec<Rule>, Vec<Vec<u8>>) {
+    let mut rules = Vec::new();
+    let mut manuals = Vec::new();
+    for line in input.lines() {
+        if line.contains("|") {
+            let parts: Vec<u8> = line
+                .split("|")
+                .into_iter()
+                .map(|v| v.parse::<u8>().unwrap())
+                .collect();
+            if parts.len() == 2 {
+                let rule = Rule {
+                    first: parts[0],
+                    second: parts[1],
+                };
+                rules.push(rule);
+            } else {
+                panic!("Line {:?} could not be parsed!", line)
+            }
+        } else if line.contains(",") {
+            let parts: Vec<u8> = line
+                .split(",")
+                .into_iter()
+                .map(|v| v.parse::<u8>().unwrap())
+                .collect();
+            manuals.push(parts);
+        }
+    }
+    let mut manuals_with_dups = Vec::new();
+    for (i, pages) in manuals.clone().into_iter().enumerate() {
+        if pages.len() > Vec::from_iter(pages.into_iter().unique()).len() {
+            manuals_with_dups.push(i);
+        }
+    }
+    if manuals_with_dups.len() == 0 {
+        println!("No duplicate pages in any manuals!");
+    } else {
+        println!(
+            "Following manual indices have duplicates: {:?}",
+            manuals_with_dups
+        );
+    }
+    (rules, manuals)
+}
+
+fn check_rule(pages: &Vec<u8>, rule: &Rule) -> bool {
+    if !(pages.contains(&rule.first) && pages.contains(&rule.second)) {
+        return true;
+    }
+    let pos_first = pages.iter().position(|&v| v == rule.first);
+    let pos_second = pages.iter().position(|&v| v == rule.second);
+    if pos_first > pos_second {
+        return false;
+    }
+    true
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let (rules, manuals) = parse_input(input);
+    let mut out = 0u64;
+    for manual in manuals {
+        let mut rules_ok = true;
+        for rule in &rules {
+            if !check_rule(&manual, &rule) {
+                rules_ok = false;
+                break;
+            }
+        }
+        if rules_ok {
+            out += manual[manual.len() / 2] as u64;
+        }
+    }
+    Some(out)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +100,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(143));
     }
 
     #[test]
